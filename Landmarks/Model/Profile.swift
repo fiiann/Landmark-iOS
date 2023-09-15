09a4aa0d700a40b640a49cd78bebc1bff5b5e1a9
Garfianto <fianto74@gmail.com>
2023-09-15 20:48:25 +0700

    working with ui control

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,30 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Garfianto Dwi Cahyadi on 14/09/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+
+    static let `default` = Profile(username: "fiiann")
+
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+
+        var id: String { rawValue }
+    }
+}
